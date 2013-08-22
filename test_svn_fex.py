168e813bbcc0db632db98ba356a0edf0542f7d13
Martin Panter <vadmium  gmailcom>
2013-08-22 08:24:39 +0000

    Subversion dump formatting using email.message.Message() class

--- test_svn_fex.py
+++ test_svn_fex.py
@@ -9,6 +9,14 @@ import os.path
 from urllib import pathname2url
 import runpy
 from subprocess import Popen
+from subvertpy.properties import (
+    PROP_REVISION_DATE,
+    PROP_REVISION_AUTHOR,
+    PROP_REVISION_LOG,
+)
+from email.message import Message
+from io import BytesIO
+from email.generator import Generator
 
 class Test(TestCase):
     def setUp(self):
@@ -25,66 +33,45 @@ class Test(TestCase):
         rmtree(self.dir)
         TestCase.tearDown(self)
     
-    def test_modify_branch(self):
-        """Modification of branch directory properties"""
+    def make_repo(self, revs):
         repo = os.path.join(self.dir, "repo")
         subprocess.check_call(("svnadmin", "create", repo))
         proc = Popen(("svnadmin", "load", "--quiet", repo),
             stdin=subprocess.PIPE)
-        proc.communicate(b"""\
-SVN-fs-dump-format-version: 2
-
-UUID: 00000000-0000-0000-0000-000000000000
-
-Revision-number: 1
-Prop-content-length: 73
-Content-length: 73
-
-K 8
-svn:date
-V 27
-1970-01-01T00:00:00.000000Z
-K 7
-svn:log
-V 0
-
-PROPS-END
-
-Node-action: add
-Node-kind: dir
-Node-path: trunk
-
-Revision-number: 2
-Prop-content-length: 73
-Content-length: 73
-
-K 8
-svn:date
-V 27
-1970-01-01T00:00:00.000000Z
-K 7
-svn:log
-V 0
-
-PROPS-END
-
-Node-action: change
-Node-kind: dir
-Node-path: trunk
-Prop-content-length: 29
-Content-length: 29
-
-K 4
-name
-V 5
-value
-PROPS-END
-
-""")
+        dump_message(proc.stdin, (("SVN-fs-dump-format-version", "2"),))
+        dump_message(proc.stdin, (
+            ("UUID", "00000000-0000-0000-0000-000000000000"),))
+        
+        for (i, rev) in enumerate(revs, 1):
+            props = {
+                PROP_REVISION_DATE: "1970-01-01T00:00:00.000000Z",
+                PROP_REVISION_LOG: "",
+            }
+            props.update(rev.get("props", ()))
+            dump_message(proc.stdin, (("Revision-number", str(i)),), props)
+            
+            for node in rev.get("nodes", {}):
+                headers = list()
+                for name in ("action", "kind", "path"):
+                    value = node.get(name)
+                    if value is not None:
+                        headers.append(("Node-" + name, value))
+                dump_message(proc.stdin, headers, node.get("props"))
+        
+        proc.communicate()
         if proc.returncode:
             raise SystemExit(proc.returncode)
         #~ subprocess.check_call(("svnadmin", "dump", "--quiet", repo))
-        
+        return repo
+    
+    def test_modify_branch(self):
+        """Modification of branch directory properties"""
+        repo = self.make_repo((
+            dict(nodes=(dict(action="add", path="trunk", kind="dir"),)),
+            dict(nodes=(
+                dict(action="change", path="trunk", props={"name": "value"}),
+            )),
+        ))
         url = "file://{}/trunk".format(pathname2url(repo))
         export = os.path.join(self.dir, "export")
         self.svn_fex["main"](url, export, "ref", root="", quiet=True)
@@ -110,42 +97,11 @@ git-svn-id: /trunk@2 00000000-0000-0000-0000-000000000000
     
     def test_authors(self):
         """Authors mapping"""
-        repo = os.path.join(self.dir, "repo")
-        subprocess.check_call(("svnadmin", "create", repo))
-        proc = Popen(("svnadmin", "load", "--quiet", repo),
-            stdin=subprocess.PIPE)
-        proc.communicate(b"""\
-SVN-fs-dump-format-version: 2
-
-UUID: 00000000-0000-0000-0000-000000000000
-
-Revision-number: 1
-Prop-content-length: 98
-Content-length: 98
-
-K 8
-svn:date
-V 27
-1970-01-01T00:00:00.000000Z
-K 7
-svn:log
-V 0
-
-K 10
-svn:author
-V 4
-user
-PROPS-END
-
-Node-action: add
-Node-kind: file
-Node-path: file
-
-""")
-        if proc.returncode:
-            raise SystemExit(proc.returncode)
-        #~ subprocess.check_call(("svnadmin", "dump", "--quiet", repo))
-        
+        repo = self.make_repo((
+            dict(props={PROP_REVISION_AUTHOR: "user"}, nodes=(
+                dict(action="add", path="file", kind="file"),
+            )),
+        ))
         url = "file://{}".format(pathname2url(repo))
         export = os.path.join(self.dir, "export")
         
@@ -154,6 +110,27 @@ Node-path: file
             print("user = user <user>", file=file)
         
         self.svn_fex["main"](url, export, "ref", authors=authors, quiet=True)
+
+def dump_message(file, headers, props=None):
+    msg = Message()
+    for (name, value) in headers:
+        msg[name] = value
+    
+    if props is not None:
+        payload = BytesIO()
+        for (key, value) in props.items():
+            print(b"K", len(key), file=payload)
+            print(key, file=payload)
+            print(b"V", len(value), file=payload)
+            print(value, file=payload)
+        print(b"PROPS-END", file=payload)
+        payload = payload.getvalue()
+        
+        msg["Prop-content-length"] = str(len(payload))
+        msg["Content-length"] = str(len(payload))
+        msg.set_payload(payload)
+    
+    Generator(file, mangle_from_=False).flatten(msg)
 
 if __name__ == "__main__":
     import unittest
