0c7e20ddd70ffd83e6dd1d4b02b4cff34e85becc
Martin Panter <vadmium  gmailcom>
2013-08-26 10:30:26 +0000

    Get Git to parse the output to avoid depending on file order
    The test was failing depending on the file shuffling order. Workaround by
    importing into Git and verifying the final Git hash.

--- test_svn_fex.py
+++ test_svn_fex.py
@@ -149,49 +149,15 @@ D file
             )),
         ))
         url = "file://{}".format(pathname2url(repo))
-        export = os.path.join(self.dir, "export")
-        self.svn_fex["Repo"](url, "ref", file=export, root="", quiet=True)
-        with open(export, "rb") as export:
-            self.assertMultiLineEqual(export.read(), b"""\
-blob
-mark :1
-data 0
-
-blob
-mark :2
-data 0
-
-commit refs/ref
-committer (no author) <(no author)@00000000-0000-0000-0000-000000000000> 0 +0000
-data 54
-
-
-git-svn-id: @1 00000000-0000-0000-0000-000000000000
-
-M 644 :1 file1
-M 644 :2 file2
-
-blob
-mark :1
-data 6
-mod 1
-
-blob
-mark :2
-data 6
-mod 2
-
-commit refs/ref
-committer (no author) <(no author)@00000000-0000-0000-0000-000000000000> 0 +0000
-data 54
-
-
-git-svn-id: @2 00000000-0000-0000-0000-000000000000
-
-M 644 :1 file1
-M 644 :2 file2
-
-""")
+        git = os.path.join(self.dir, "git")
+        subprocess.check_call(("git", "init", "--quiet", "--", git))
+        script = 'cd "$1" && git fast-import --quiet'
+        importer = ("sh", "-c", script, "--", git)
+        self.svn_fex["Repo"](url, "heads/master", importer=importer, root="",
+            quiet=True)
+        cmd = ("git", "rev-parse", "--verify", "refs/heads/master")
+        rev = subprocess.check_output(cmd, cwd=git).strip()
+        self.assertEqual(rev, "82aeb20279a1269f048243a603b141ee0ea204e9")
 
 def dump_message(file, headers, props=None, content=None):
     msg = Message()
