e8582ba94c26aec648b1bb1447cf5c45f4d7b3f3
Martin Panter <vadmium  gmailcom>
2013-09-05 04:18:44 +0000

    The assertEqual() methods work better with the test variable second

--- test_svn_fex.py
+++ test_svn_fex.py
@@ -81,7 +81,7 @@ class Test(TestCase):
         export = os.path.join(self.dir, "export")
         self.svn_fex["Repo"](url, "ref", file=export, root="", quiet=True)
         with open(export, "rb") as export:
-            self.assertMultiLineEqual(export.read(), b"""\
+            self.assertMultiLineEqual(b"""\
 commit refs/ref
 committer (no author) <(no author)@00000000-0000-0000-0000-000000000000> 0 +0000
 data 60
@@ -98,7 +98,8 @@ data 60
 git-svn-id: /trunk@2 00000000-0000-0000-0000-000000000000
 
 
-""")
+""",
+                export.read())
     
     def test_authors(self):
         """Authors mapping"""
@@ -126,7 +127,7 @@ git-svn-id: /trunk@2 00000000-0000-0000-0000-000000000000
         self.svn_fex["Repo"](url, "ref", file=export, root="", base_rev=1,
             quiet=True)
         with open(export, "rb") as export:
-            self.assertMultiLineEqual(export.read(), b"""\
+            self.assertMultiLineEqual(b"""\
 commit refs/ref
 committer (no author) <(no author)@00000000-0000-0000-0000-000000000000> 0 +0000
 data 54
@@ -137,7 +138,8 @@ git-svn-id: @2 00000000-0000-0000-0000-000000000000
 from ref
 D file
 
-""")
+""",
+                export.read())
     
     def test_multiple(self):
         """Modification of multiple files"""
@@ -160,7 +162,7 @@ D file
             quiet=True)
         cmd = ("git", "rev-parse", "--verify", "refs/heads/master")
         rev = subprocess.check_output(cmd, cwd=git).strip()
-        self.assertEqual(rev, "82aeb20279a1269f048243a603b141ee0ea204e9")
+        self.assertEqual("82aeb20279a1269f048243a603b141ee0ea204e9", rev)
     
     def test_executable(self):
         """Order of setting file mode and contents should not matter"""
@@ -168,7 +170,7 @@ D file
         export = os.path.join(self.dir, "export")
         self.svn_fex["Repo"]("file:///repo", "ref", file=export, quiet=True)
         with open(export, "rb") as export:
-            self.assertMultiLineEqual(export.read(), b"""\
+            self.assertMultiLineEqual(b"""\
 blob
 mark :1
 data 0
@@ -207,7 +209,8 @@ git-svn-id: file:///repo@2 00000000-0000-0000-0000-000000000000
 M 755 :1 file1
 M 755 :2 file2
 
-""")
+""",
+                export.read())
 
 def executable_add(editor):
     root = editor.open_root(0)
