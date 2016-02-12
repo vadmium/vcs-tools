7cdd69f85785f2baa49620578a9d1991431a97a9
Martin Panter <vadmium  gmailcom>
2016-02-12 09:54:31 +0000

    Split off show_copies() function

--- svnlog.py
+++ svnlog.py
@@ -10,44 +10,48 @@ def main(*, only_to="/", only_from="/", not_from=()):
     only_from = parse_path(only_from)
     
     for [rev, copies] in iter_svn_copies(stdin.buffer):
-        if copies is None:
+        show_copies(rev, copies,
+            only_to=only_to, only_from=only_from, not_from=not_from)
+
+def show_copies(rev, copies, *, only_to, only_from, not_from):
+    if copies is None:
+        return
+    for [path, from_rev, from_path] in copies:
+        if (
+            path[:len(only_to)] != only_to[:len(path)] or
+            from_path[:len(only_from)] != only_from[:len(from_path)] or
+            any(from_path[:len(x)] == x for x in not_from)
+        ):
             continue
-        for [path, from_rev, from_path] in copies:
-            if (
-                path[:len(only_to)] != only_to[:len(path)] or
-                from_path[:len(only_from)] != only_from[:len(from_path)] or
-                any(from_path[:len(x)] == x for x in not_from)
-            ):
-                continue
-            
-            max_common = min(len(path), len(from_path))
-            for i in range(max_common):
-                if path[i] != from_path[i]:
-                    break
-            else:
-                i = max_common
-            prefix = path[:i]
-            
-            max_common -= i
-            for i in range(max_common):
-                if path[-1 - i] != from_path[-1 - i]:
-                    break
-            else:
-                i = max_common
-            suffix = path[len(path) - i:]
-            
-            path = path[len(prefix):len(path) - len(suffix)]
-            path = "/".join(path)
-            from_path = from_path[len(prefix):len(from_path) - len(suffix)]
-            from_path = "/".join(from_path)
-            if prefix:
-                prefix = "/" + "/".join(prefix)
-            else:
-                prefix = ""
-            copy = f"{prefix}/({path}@{rev} <- {from_path}@{from_rev})"
-            if suffix:
-                copy = f"{copy}/{'/'.join(suffix)}"
-            print(copy)
+        
+        max_common = min(len(path), len(from_path))
+        for i in range(max_common):
+            if path[i] != from_path[i]:
+                break
+        else:
+            i = max_common
+        prefix = path[:i]
+        
+        max_common -= i
+        for i in range(max_common):
+            if path[-1 - i] != from_path[-1 - i]:
+                break
+        else:
+            i = max_common
+        suffix = path[len(path) - i:]
+        
+        path = path[len(prefix):len(path) - len(suffix)]
+        path = "/".join(path)
+        from_path = from_path[len(prefix):len(from_path) - len(suffix)]
+        from_path = "/".join(from_path)
+        if prefix:
+            prefix = "/" + "/".join(prefix)
+        else:
+            prefix = ""
+        copy = f"{prefix}/({path}@{rev} <- {from_path}@{from_rev})"
+        if suffix:
+            copy = f"{copy}/{'/'.join(suffix)}"
+        print(copy)
 
 def iter_svn_copies(stream):
     stream = pulldom.parse(stream)
